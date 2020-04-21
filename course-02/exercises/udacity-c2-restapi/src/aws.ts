f156b6c320b5c25ed71ce4b50b0aa06fda282061
Chris Casson <cassonchris723@gmail.com>
2020-04-21 18:05:46 -0500

    Put config values in environment variables

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -4,13 +4,13 @@ import { config } from './config/config';
 const c = config.dev;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+var credentials = new AWS.SharedIniFileCredentials({profile: c.aws.profile});
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
-  region: c.aws_region,
-  params: {Bucket: c.aws_media_bucket}
+  region: c.aws.region,
+  params: {Bucket: c.aws.media_bucket}
 });
 
 
@@ -25,7 +25,7 @@ export function getGetSignedUrl( key: string ): string{
   const signedUrlExpireSeconds = 60 * 5;
 
   const url = s3.getSignedUrl('getObject', {
-      Bucket: c.aws_media_bucket,
+      Bucket: c.aws.media_bucket,
       Key: key,
       Expires: signedUrlExpireSeconds
     });
@@ -44,7 +44,7 @@ export function getPutSignedUrl( key: string ){
     const signedUrlExpireSeconds = 60 * 5;
 
     const url = s3.getSignedUrl('putObject', {
-      Bucket: c.aws_media_bucket,
+      Bucket: c.aws.media_bucket,
       Key: key,
       Expires: signedUrlExpireSeconds
     });
